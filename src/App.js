7c2c001126e73d10b562cc86ae41370388a90d5c
Larry <50931759+Turkinolith@users.noreply.github.com>
2020-02-03 15:31:02 -0700

    Staged support for login and registering. TODO: style Navbar, handle routing and forms.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Lists from "./components/Lists";
 import Background from "./components/Background";
+import NavBar from "./components/common/navbar";
+import LoginForm from "./components/loginForm";
+import RegisterForm from "./components/registerForm";
+import NotFound from "./components/notFound";
 import "./sass/app.scss";
 
 class App extends Component {
@@ -9,6 +14,14 @@ class App extends Component {
     return (
       <React.Fragment>
         <Background />
+        <NavBar />
+        <Switch>
+          <Route path="/login" component={LoginForm} />
+          <Route path="/register" component={RegisterForm} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/Lists" />
+          <Redirect to="/not-found" />
+        </Switch>
         <main className="container">
           <Lists />
         </main>
---
+++ src/components/registerForm.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import Joi from "@hapi/joi";
+import Form from "./common/Form";
+
+class RegisterForm extends Form {
+  state = {
+    data: { username: "", password: "", nameuser: "" },
+    errors: {}
+  };
+
+  schema = {
+    username: Joi.string()
+      .email({
+        minDomainSegments: 2,
+        tlds: { allow: ["com", "net", "org", "edu"] }
+      })
+      .required()
+      .label("Username"),
+    password: Joi.string()
+      .min(5)
+      .required()
+      .label("Password"),
+    nameuser: Joi.string()
+      .required()
+      .label("Name")
+  };
+
+  doSubmit = () => {
+    // TODO: call the server, save the changes, and then redirect the user to a different page.
+    console.log("Registration Submitted");
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>Register</h1>
+        <form onSubmit={this.handleSubmit}>
+          {this.renderInput("username", "Username")}
+          {this.renderInput("password", "Password", "password")}
+          {this.renderInput("nameuser", "Name")}
+          {this.renderButton("Register")}
+        </form>
+      </div>
+    );
+  }
+}
+
+export default RegisterForm;
